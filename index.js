c26b44bc9fb623b907c692bd26869da18ea36e40
liubiqu <liubiqu@qq.com>
2021-05-02 07:33:05 +0800

    代码格式化

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 /**
  * * * *【简介】* * * *
- * 
+ *
  * TribeDB 是一个mysql分布式集群储存系统。
  * 采用分库分表的方式，在处理海量数据时获得更加优越的性能。
- * 
+ *
  * Github    ：https://github.com/yangjiePro/TribeDB
  * Home Page ：http://yangjiePro.github.com/TribeDB
  * API docs  ：http://yangjiePro.github.com/TribeDB
- * 
+ *
  * * * *【依赖】* * * *
- * 
+ *
  * TribeDB 基于 node-mysql 模块
  * Github：https://github.com/felixge/node-mysql
  * 安装：  npm install mysql
- * 
+ *
  * * * *【作者】* * * *
- * 
+ *
  * 作者   ：杨捷
  * QQ     ：446342398
- * 邮箱   ：yangjie@jojoin.com 
+ * 邮箱   ：yangjie@jojoin.com
  * 主页   ：http://jojoin.com/user/1
  * Github ：https://github.com/yangjiePro
- * 
+ *
  * 欢迎交流讨论或提交新的代码！
  */
 
@@ -36,14 +36,12 @@ exports.pool = require('./lib/pool.js');
 exports.Query = require('./lib/query.js');
 
 
-
 /********调试函数*********/
 
 
-
 // 全局 die
-global.die = function(stuff){
-    if(stuff!==undefined){
+global.die = function(stuff) {
+    if (stuff !== undefined) {
         console.log(stuff);
     }
     process.exit(1);
